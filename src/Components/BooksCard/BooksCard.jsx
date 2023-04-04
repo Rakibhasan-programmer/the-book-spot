716bb47a3f8725d7a96c850bf8ec37eafa3fb19e
unknown <rakibulhasan.seu.cse@gmail.com>
2023-04-04 07:49:44 +0600

    book details page

--- src/Components/BooksCard/BooksCard.jsx
+++ src/Components/BooksCard/BooksCard.jsx
@@ -1,21 +1,24 @@
 import { Card } from "flowbite-react";
 import React from "react";
+import { Link } from "react-router-dom";
 
 const BooksCard = ({ book }) => {
   const {title, image, price} = book;
   return (
     <>
-      <Card
-        imgAlt="Meaningful alt text for an image that is not purely decorative"
-        imgSrc={image}
-      >
-        <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-          {title}
-        </h5>
-        <p className="font-normal text-gray-700 dark:text-gray-400">
-          Price: {price}
-        </p>
-      </Card>
+      <Link to={`../store/${book.isbn13}`}>
+        <Card
+            imgAlt="Meaningful alt text for an image that is not purely decorative"
+            imgSrc={image}
+        >
+            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            {title}
+            </h5>
+            <p className="font-normal text-gray-700 dark:text-gray-400">
+            Price: {price}
+            </p>
+        </Card>
+      </Link>
     </>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import ErrorPage from './Components/ErrorPage/ErrorPage'
-import BookStore from './Components/BookStore/BookStore'
-import HomePage from './Components/HomePage/HomePage'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import ErrorPage from "./Components/ErrorPage/ErrorPage";
+import BookStore from "./Components/BookStore/BookStore";
+import HomePage from "./Components/HomePage/HomePage";
+import BookDetails from "./Components/BookDetails/BookDetails";
 
 const router = createBrowserRouter([
   {
@@ -14,20 +15,26 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />,
     children: [
       {
-        path: '/',
-        element: <HomePage />
+        path: "/",
+        element: <HomePage />,
       },
       {
         path: "store",
         element: <BookStore />,
-        loader: () => fetch('https://api.itbook.store/1.0/new'),
-      }
-    ]
-  }
+        loader: () => fetch("https://api.itbook.store/1.0/new"),
+      },
+      {
+        path: "store/:id",
+        element: <BookDetails />,
+        loader: ({ params }) =>
+          fetch(`https://api.itbook.store/1.0/books/${params.id}`),
+      },
+    ],
+  },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
